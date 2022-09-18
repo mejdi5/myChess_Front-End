3da98d12c5d22aa23e3ee9d4fa2579d94316bd82
Mejdi Ben Ammou <mejdi.ben.ammou@gmail.com>
2022-09-18 18:15:50 +0200

    deploy

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://mychess1.herokuapp.com/"
+const baseUrl = process.env.NODE_ENV === 'development' ? "http://localhost:5000" : "https://mychess.adaptable.app"
 
 export const Axios = axios.create({
     baseURL: baseUrl
